1d9efd43f0c5f2d9efb4645efe061fd12c9f313d
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-04-07 15:51:55 -0300

    Create DivBuilder.cs

---
+++ Rebec/Builders/DivBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using Rebec.Interfaces;
+
+namespace Rebec.Builders
+{
+    public class DivBuilder : IBuilder
+    {
+        public List<IBuilderStyle> Style { get; } = new List<IBuilderStyle>();
+        
+        public string Id { get; set; }
+        
+        public List<INode> Children { get; } = new List<INode>();
+
+        public IElement Build()
+        {
+            var htmlDivElement = new HtmlParser().ParseDocument(string.Empty)
+                .CreateElement<IHtmlDivElement>();
+
+            if (!string.IsNullOrEmpty(Id))
+            {
+                htmlDivElement.Id = Id;
+            }
+
+            foreach (var child in Children)
+            {
+                htmlDivElement.AppendChild(child);
+            }
+
+            if (Style.Any(i => !i.IsInline))
+                htmlDivElement.SetAttribute("class",
+                    Style.Where(i => !i.IsInline).Select(i => i.Class).Aggregate((x, y) => $"{x};{y}"));
+
+            if (Style.Any(i => i.IsInline))
+                htmlDivElement.SetAttribute("style",
+                    Style.Where(i => i.IsInline).Select(i => i.Class).Aggregate((x, y) => $"{x};{y}"));
+
+            return htmlDivElement;
+        }
+
+        public DivBuilder WithStyle(IBuilderStyle style)
+        {
+            Style.Add(style);
+            return this;
+        }
+
+        public DivBuilder WithChild(INode child)
+        {
+            Children.Add(child);
+            return this;
+        }
+    }
+}
