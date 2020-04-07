c1bff1472609de627fe2027687b47c1726265757
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-04-07 15:52:35 -0300

    Adds EzPDF project

---
+++ Rebec.EzPDF/PdfRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using EzPDF;
+using Rebec.Interfaces;
+
+namespace Rebec.EzPDF
+{
+    public class PdfRenderer : IHtmlRenderer
+    {
+        public Task<byte[]> RenderHtml(IReportResult reportResult)
+        {
+            var pdfRenderer = new HtmlRenderer();
+            return pdfRenderer.Render(reportResult.Html);
+        }
+    }
+}
